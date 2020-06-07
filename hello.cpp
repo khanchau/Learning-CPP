fe843b464533801d1df3e86ae9f0e9372b28ae3b
Khanchau <foxarticfox@gmail.com>
2020-06-07 15:02:42 -0700

    Updating comments, fixing some bad practices

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,36 @@
+/* 
+    Compile with g++ filename -o compiledname
+    
+    Ex. g++ hello.cpp -o hello
+    
+    If the "-o" option is left out the generated 
+    file will default to "a"
+*/
+
 // Allows for read or write to the standard input/output streams.
 #include <iostream> 
-// Allows the compiler to use symbol names defined in the std namespace.
-using namespace std; 
+/* 
+    Allows the compiler to use symbol names defined in the std namespace.
+    
+    using namespace std; 
+
+    EDIT: This line of code is bad practice as you could have potential
+    conflicts in your global namespace should function names and their
+    parameters match from different libraries.
+*/
+
+/* 
+    Allows the omittance of std:: when using cout, but 
+    without bringing in the entire std namespace.
+*/
+using std::cout;
 
 // main class that is used to execute your code.
+
+/*
+    Prints out Hello World
+    @return int
+*/
 int main() {
     //cout is the standard output stream used to print to the screen.
     cout << "Hello World"; 
@@ -14,12 +41,3 @@ int main() {
     */
     return 0;
 }
-
-/* 
-    Compile with g++ filename -o compiledname
-    
-    Ex. g++ basics.cpp -o basics
-    
-    If the "-o" option is left out the generated 
-    file will default to "a"
-*/
---
+++ twosums.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cout;
+
+
+/*
+    Iterates through a given array to see if contents
+    add up to a specified target value.
+    @param arr[]: array to search content of.
+    @param target: integer value to check sums of array contents against.
+    @return: An array containing the indices of the array that add up to
+             the target.
+
+    6/7/20 - Looking into vectors instead of arrays.  
+*/
+int twosums(int arr[], int target) {
+    return 0;
+}
+
+int main() {
+    int arr[] = {0,1,2,3};
+    twosums(arr, 5);
+    return 0;
+}
