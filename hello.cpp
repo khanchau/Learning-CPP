73090b8f72e759a2e6ec20db24935a88da6f725c
Khanchau <foxarticfox@gmail.com>
2020-06-07 13:47:51 -0700

    Added explainatory comments

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,17 @@
-#include <iostream>
-using namespace std;
+// Allows for read or write to the standard input/output streams.
+#include <iostream> 
+// Allows the compiler to use symbol names defined in the std namespace.
+using namespace std; 
 
 // main class that is used to execute your code.
 int main() {
     //cout is the standard output stream used print to the screen.
     cout << "Hello World"; 
+    /* 
+        The method is declared as "int main" and thus should
+        return an integer value. Generally, 0 indicates that
+        the program ran successfully.
+    */
     return 0;
 }
 
