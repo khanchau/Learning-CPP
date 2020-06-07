c56c5e8cf44206cb1c56fe2c061de6c8a3d98296
Khanchau <foxarticfox@gmail.com>
2020-06-07 13:33:13 -0700

    Printing Hello World

---
+++ basics.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// main class that is used to execute your code.
+int main() {
+    //cout is the standard output stream used print to the screen.
+    cout << "Hello World"; 
+    return 0;
+}
+
+/* 
+    Compile with g++ filename -o compiledname
+    
+    Ex. g++ basics.cpp -o basics
+    
+    If the "-o" option is left out the generated 
+    file will default to "a"
+*/
