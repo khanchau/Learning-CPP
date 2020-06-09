717a66a42dbf8a83525e0c90b1cb583be1baaba2
Khanchau <foxarticfox@gmail.com>
2020-06-08 19:32:40 -0700

    Added hashing notes, setting up hash problem

---
+++ hashes.cpp
@@ -0,0 +1,6 @@
+/*
+    Hash set: Set data structure, stores no repeated values.
+    Hash map: Map data structure, stores key and value pairs.
+    
+    - General idea of hash table is mapping keys to buckets.
+*/
